7797999c91e6c16c45e34a6bffb8e4e558b79d31
Brian Benchoff <benchoff@gmail.com>
2020-01-28 12:29:20 -0800

    atmel studio project

--- Firmware/DumbBadgeFirmware/src/main.c
+++ Firmware/DumbBadgeFirmware/src/main.c
@@ -13,8 +13,13 @@
 #define LCD_DC	PORT_PB23
 #define LCD_RD	PORT_PB16
 
+
+
 /** VARIABLES *****************************************************************/
 
+char fore_Color_High, fore_Color_Low, back_Color_High, back_Color_Low;
+uint16_t display_X_size = 479;
+uint16_t display_Y_size = 799;
 
 
 /** LOCAL PROTOTYPES **********************************************************/
@@ -25,6 +30,20 @@ void LCD_Write_COM16(char VH, char VL);
 void LCD_Write_COM8(char VL);
 void LCD_Write_DATA16(char VH, char VL);
 void LCD_Write_DATA8(char VL);
+
+void setColorRGB(char r, char g, char b);
+void setColorHex(uint16_t color);
+void setBackColorRGB(char r, char g, char b);
+void setBackColorHex(uint16_t color);
+
+void clrScr(void);
+void clrXY(void);
+void setXY(uint16_t x1, uint16_t y1, uint16_t x2, uint16_t y2);
+void SwapUint16(uint16_t i, uint16_t j);
+
+void fillRect(int x1, int y1, int x2, int y2);
+void LCD_Fast_Fill(int ch, int cl, int pix);
+
 
 int main (void)
 {
@@ -52,7 +71,29 @@ int main (void)
 
 	/* This skeleton code simply sets the LED to the state of the button. */
 	while (1) {
+		
+		clrScr();
+		fillRect(0, 466, 799, 479);
+		
+		delay_ms(500);
+		
+		setColorRGB(255,255,255);
+		setBackColorRGB(255,0,0);
+		
+		fillRect(0, 466, 799, 479);
+		
+		delay_ms(500);
+		
+		setColorRGB(0,255,255);
+		setBackColorRGB(255,255,0);
+		
+		fillRect(0, 466, 799, 479);
+		
+		delay_ms(500);
+		
 		/* Is button pressed? */
+		/* this is a sanity check and came from the default
+		microchip 'set the clocks and blink a LED' project */
 		if (port_pin_get_input_level(BUTTON_0_PIN) == BUTTON_0_ACTIVE) {
 			/* Yes, so turn LED on. */
 			delay_ms(1000);
@@ -63,6 +104,142 @@ int main (void)
 		}
 	}
 }
+
+void fillRect(int x1, int y1, int x2, int y2)
+{
+	if (x1>x2)
+	{
+		SwapUint16(x1, x2);
+	}
+	if (y1>y2)
+	{
+		SwapUint16(y1, y2);
+	}
+	
+	REG_PORT_OUTCLR1 = LCD_CS;
+	setXY(x1, y1, x2, y2);
+	REG_PORT_OUTSET1 = LCD_DC;
+	LCD_Fast_Fill(fore_Color_High, fore_Color_Low, (((x2-x1)+1)*((y2-y1)+1)));
+	REG_PORT_OUTSET1 = LCD_CS;
+}
+
+void LCD_Fast_Fill(int ch, int cl, int pix)
+{
+	int blocks;
+
+	REG_PORT_OUTCLR1 = 0x0000ffff;
+	REG_PORT_OUTSET1 = (ch << 8) | cl;
+
+	blocks = pix/16;
+	for (int i=0; i<blocks; i++)
+	{
+		REG_PORT_OUTCLR1 = LCD_WR;
+		REG_PORT_OUTSET1 = LCD_WR;
+		
+		REG_PORT_OUTCLR1 = LCD_WR;
+		REG_PORT_OUTSET1 = LCD_WR;
+		
+		REG_PORT_OUTCLR1 = LCD_WR;
+		REG_PORT_OUTSET1 = LCD_WR;
+		
+		REG_PORT_OUTCLR1 = LCD_WR;
+		REG_PORT_OUTSET1 = LCD_WR;
+		
+		REG_PORT_OUTCLR1 = LCD_WR;
+		REG_PORT_OUTSET1 = LCD_WR;
+		
+		REG_PORT_OUTCLR1 = LCD_WR;
+		REG_PORT_OUTSET1 = LCD_WR;
+		
+		REG_PORT_OUTCLR1 = LCD_WR;
+		REG_PORT_OUTSET1 = LCD_WR;
+		
+		REG_PORT_OUTCLR1 = LCD_WR;
+		REG_PORT_OUTSET1 = LCD_WR;
+		
+		REG_PORT_OUTCLR1 = LCD_WR;
+		REG_PORT_OUTSET1 = LCD_WR;
+		
+		REG_PORT_OUTCLR1 = LCD_WR;
+		REG_PORT_OUTSET1 = LCD_WR;
+		
+		REG_PORT_OUTCLR1 = LCD_WR;
+		REG_PORT_OUTSET1 = LCD_WR;
+		
+		REG_PORT_OUTCLR1 = LCD_WR;
+		REG_PORT_OUTSET1 = LCD_WR;
+		
+		REG_PORT_OUTCLR1 = LCD_WR;
+		REG_PORT_OUTSET1 = LCD_WR;
+		
+		REG_PORT_OUTCLR1 = LCD_WR;
+		REG_PORT_OUTSET1 = LCD_WR;
+		
+		REG_PORT_OUTCLR1 = LCD_WR;
+		REG_PORT_OUTSET1 = LCD_WR;
+		
+		REG_PORT_OUTCLR1 = LCD_WR;
+		REG_PORT_OUTSET1 = LCD_WR;
+	}
+	
+	if ((pix % 16) != 0)
+		for (int i=0; i<(pix % 16)+1; i++)
+		{
+			REG_PORT_OUTCLR1 = LCD_WR;
+			REG_PORT_OUTSET1 = LCD_WR;
+		}
+}
+	
+
+void SwapUint16(uint16_t i, uint16_t j)
+{
+	uint16_t t = i; 
+	i = j; 
+	j = t;
+}
+
+void clrScr(void)
+{
+	REG_PORT_OUTCLR1 = LCD_CS;
+	clrXY();
+}
+
+void clrXY(void)
+{
+	setXY(0,0,display_Y_size,display_X_size);
+}
+
+void setXY(uint16_t x1, uint16_t y1, uint16_t x2, uint16_t y2)
+{
+		
+	SwapUint16(x1, y1);
+	SwapUint16(x2, y2);
+	y1=display_Y_size-y1;
+	y2=display_Y_size-y2;
+	SwapUint16(y1, y2);
+	
+	LCD_Write_COM16(0x2a,0x00);
+	LCD_Write_DATA8(x1>>8);
+	LCD_Write_COM16(0x2a,0x01);
+	LCD_Write_DATA8(x1);
+	LCD_Write_COM16(0x2a,0x02);
+	LCD_Write_DATA8(x2>>8);
+	LCD_Write_COM16(0x2a,0x03);
+	LCD_Write_DATA8(x2);
+
+
+	LCD_Write_COM16(0x2b,0x00);
+	LCD_Write_DATA8(y1>>8);
+	LCD_Write_COM16(0x2b,0x01);
+	LCD_Write_DATA8(y1);
+	LCD_Write_COM16(0x2b,0x02);
+	LCD_Write_DATA8(y2>>8);
+	LCD_Write_COM16(0x2b,0x03);
+	LCD_Write_DATA8(y2);
+
+	LCD_Write_COM16(0x2c,0x00);
+}
+
 
 void LCD_Write_Bus(char VH, char VL)
 {
@@ -578,8 +755,31 @@ void InitLCD(void)
 	
 	REG_PORT_OUTSET1 = LCD_CS;
 	
-	//no idea what this shit does so whatever
-	//	setColor(255, 255, 255);
-	//	setBackColor(0, 0, 0);
+	setColorRGB(255, 255, 255);
+	setBackColorRGB(0, 0, 0);
 	
+}
+
+void setColorRGB(char r, char g, char b)
+{
+	fore_Color_High = ((r&248)|g>>5);
+	fore_Color_Low = ((g&28)<<3|b>>3);
+}
+
+void setColorHex(uint16_t color)
+{
+	fore_Color_High = (color >> 8);
+	fore_Color_Low = (color & 0xFF);
+}
+
+void setBackColorRGB(char r, char g, char b)
+{
+	back_Color_High = ((r&248)|g>>5);
+	back_Color_Low = ((g&28)<<3|b>>3);
+}
+
+void setBackColorHex(uint16_t color)
+{
+	back_Color_High = (color >> 8);
+	back_Color_Low = (color & 0xFF);
 }
