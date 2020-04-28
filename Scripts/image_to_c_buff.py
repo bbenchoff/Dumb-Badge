d0d06958f97ffa479e159305bd63dba061cc17db
kscottz <katherineAScott@gmail.com>
2020-04-27 22:47:16 -0700

    This is still crap but it should work.

--- Scripts/image_to_c_buff.py
+++ Scripts/image_to_c_buff.py
@@ -3,6 +3,33 @@ import numpy as np
 import argparse
 
 
+def post_process_chip(chip, thresh=128):
+    # threshold and invert
+    garbage,ret_val = cv2.threshold(chip,thresh,255,cv2.THRESH_BINARY_INV)
+    return ret_val
+
+def chip_to_packed_char(chip):
+    bits = 8
+    chip = chip / 255
+    flat = chip.flatten("C")
+    sz = int(flat.size / bits)
+    int_series = []
+    for idx in range(0,sz):
+        start = idx*bits
+        stop = (idx*bits)+bits
+        b = flat[start:stop]
+        print(b)
+        bstr = ""
+        for ab in b:
+            bstr += str(int(ab))
+        print(bstr)
+        val = int(bstr,2)
+        int_series.append(val)
+    print(int_series)
+    print([hex(h) for h in int_series])
+    return int_series
+
+
 def split_source_image(infile, block_size=[20,10]):
     img = cv2.imread(infile)
     sz = img.shape
@@ -17,10 +44,18 @@ def split_source_image(infile, block_size=[20,10]):
             y_start = vsz*block_size[0]
             y_stop = (vsz*block_size[0])+block_size[0]
             temp = img[y_start:y_stop,x_start:x_stop,0]
-            cv2.imwrite("./temp/{0}.png".format(hex(idx)),temp)
-            retVal[idx] = temp
+            #cv2.imwrite("./temp/{0}.png".format(hex(idx)),temp)
+            retVal[idx] = post_process_chip(temp)
             idx += 1
-            print(hex(idx))
+
+            if idx == 98:
+                t = post_process_chip(temp)
+                print(t)
+                print(chip_to_packed_char(t))
+
+
+    return retVal
+
 
 
 #    print(retVal[33])
