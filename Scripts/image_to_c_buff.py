3c747d36af517f6efd2acc367a0de2bb6a3c534e
kscottz <katherineAScott@gmail.com>
2020-05-03 12:21:04 -0700

    Fix indexing into source file.

--- Scripts/image_to_c_buff.py
+++ Scripts/image_to_c_buff.py
@@ -18,15 +18,11 @@ def chip_to_int_series(chip):
         start = idx*bits
         stop = (idx*bits)+bits
         b = flat[start:stop]
-        #print(b)
         bstr = ""
         for ab in b:
             bstr += str(int(ab))
-        #print(bstr)
         val = int(bstr,2)
         int_series.append(val)
-    #print(int_series)
-    #print([hex(h) for h in int_series])
     return int_series
 
 
@@ -37,18 +33,19 @@ def split_source_image(infile, block_size=[20,10]):
     h = sz[1]
     idx = 0
     retVal = {}
-    for hsz in range(0,int(w/block_size[1])):
-        for vsz in range(0,int(h/block_size[0])):
+    # top to bottom, left to right through the source image.
+    hc = int(w/block_size[0])-1
+    vc = int(h/block_size[1])-1
+    for hsz in range(0,hc):
+        for vsz in range(0,vc):
             x_start = hsz*block_size[1]
             x_stop = (hsz*block_size[1])+block_size[1]
             y_start = vsz*block_size[0]
             y_stop = (vsz*block_size[0])+block_size[0]
             temp = img[y_start:y_stop,x_start:x_stop,0]
             #cv2.imwrite("./temp/{0}.png".format(hex(idx)),temp)
-
             retVal[idx] = chip_to_int_series(post_process_chip(temp))
-
-            if idx == 97:
+            if idx == 127:
                 t = post_process_chip(temp)
                 print(t)
                 print(chip_to_int_series(t))
@@ -83,8 +80,6 @@ def chip_map_to_c_file(chip_map,fname):
     for k,v in chip_map.items():
         name = "character_0x{:02x}".format(k)
         c_def = int_series_to_c_string(v,name)
-        if(k == 97):
-            print(c_def)
         f.write(c_def)
     return
 
