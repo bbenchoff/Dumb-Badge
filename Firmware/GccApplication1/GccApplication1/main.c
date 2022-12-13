827ac5cea402b7ec44f19d61a36b870715db1d99
Brian Benchoff <benchoff@gmail.com>
2022-12-13 10:43:12 -0800

    update

---
+++ Firmware/GccApplication1/GccApplication1/main.c
@@ -0,0 +1,18 @@
+/*
+ * GccApplication1.c
+ *
+ * Created: 10/10/2021 9:35:44 PM
+ * Author : bench
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
